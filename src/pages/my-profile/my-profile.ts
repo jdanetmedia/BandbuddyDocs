a157efa72241952d9d25e92fb0b2f91956fb84b3
Jesper Dalsgaard <post@jdanet.dk>
2018-01-04 11:33:31 +0100

    mere post billede

--- src/pages/my-profile/my-profile.ts
+++ src/pages/my-profile/my-profile.ts
@@ -40,14 +40,11 @@ export class MyProfilePage {
     try {
       // Camera options
       const options: CameraOptions = {
-        targetWidth: 600,
-        targetHeight: 600,
         quality: 50,
         destinationType: this.camera.DestinationType.DATA_URL,
         encodingType: this.camera.EncodingType.JPEG,
         mediaType: this.camera.MediaType.PICTURE,
-        correctOrientation: true,
-        cameraDirection: 1
+        correctOrientation: true
       }
       const result = await this.camera.getPicture(options);
 
