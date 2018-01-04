1b931861c8186d37f5545244344b5ff77265d58c
Jesper Dalsgaard <post@jdanet.dk>
2018-01-04 10:47:34 +0100

    build fejl rettet?

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
+import { MyApp } from './app.component';
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
